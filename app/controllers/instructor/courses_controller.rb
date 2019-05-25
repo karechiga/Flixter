978a97af18c17f4868809f4cf034c5a0178083ff
Kyle Arechiga <kylearechiga@email.arizona.edu>
2019-05-25 02:57:51 +0000

    added more authentication

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,17 +1,16 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
   end
 
   def show
-    @course = Course.find(params[:id])
   end
 
   def create
-    @course = current_user.courses.create(course_params)
-    if @course.valid?
+    if current_course.valid?
       redirect_to instructor_course_path(@course)
     else
       render :new, status: :unprocessable_entity
@@ -19,6 +18,17 @@ class Instructor::CoursesController < ApplicationController
   end
 
   private
+
+  def require_authorization_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
