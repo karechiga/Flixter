cc9affa917a12ef08e9ade575f7783632afa5c4d
Kyle Arechiga <kylearechiga@email.arizona.edu>
2019-05-28 04:15:17 +0000

    image uploading working

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,25 +1,26 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorization_for_current_course, only: [:show]
+  before_action :require_authorized_for_current_course, only: [:show]
 
   def new
     @course = Course.new
   end
 
-  def show
-  end
-
   def create
-    if current_course.valid?
-      redirect_to instructor_course_path(current_course)
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def show
+  end
+
   private
 
-  def require_authorization_for_current_course
+  def require_authorized_for_current_course
     if current_course.user != current_user
       render plain: "Unauthorized", status: :unauthorized
     end
@@ -31,6 +32,6 @@ class Instructor::CoursesController < ApplicationController
   end
 
   def course_params
-    params.require(:course).permit(:title, :description, :cost)
+    params.require(:course).permit(:title, :description, :cost, :image)
   end
 end
