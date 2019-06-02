af71309afb00a6ff70d4578cf25020fa211b2645
Kyle Arechiga <kylearechiga@email.arizona.edu>
2019-06-02 04:21:16 +0000

    added lesson authentication

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,25 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
+
   def show
   end
 
   private
 
   helper_method :current_lesson
+  helper_method :current_course
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+  
+  def current_course
+    @current_course = current_lesson.section.course
+  end
+
+  def require_authorized_for_current_lesson
+    if !current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'Only enrolled students may view lessons.'
+    end
   end
 end
